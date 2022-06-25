0e387bbb2f4b550a40d6c6f8baf8dbeed6ac4713
Facundo-cali <facundocali1234@gmail.com>
2022-06-24 22:49:29 -0300

    ejercicio 4 curso js

---
+++ Ejercicio_4_js.js
@@ -0,0 +1,32 @@
+//Ejercicio 4 curso js
+
+let nom = "Facundo";
+let apellido = "Cali";
+
+let estudiante = nom.concat(" ", apellido,"            ")
+console.log(estudiante.length);
+
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let estuNumCarac = estudiante.length;
+
+let primLetra = estudiante.charAt(0);
+
+let ultimaLetraApellido =  apellido.charAt(3);
+
+let quitarEspacios = estudiante.replace(/ /g,"");
+
+let siNomEsta = estudiante.includes("Facu");
+
+
+
+
+
+
+
+
+
+
